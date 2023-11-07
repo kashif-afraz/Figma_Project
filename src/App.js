2f7343bde8166fcbb5fa8063c99757f7f7259369
kashif-afraz <144420266+kashif-afraz@users.noreply.github.com>
2023-11-07 16:17:30 +0500

    Completed (before responsive)

--- src/App.js
+++ src/App.js
@@ -9,58 +9,9 @@ import WhatPeopleSay from "./components/WhatPeopleSay";
 import WhyChoose from "./components/WhyChoose";
 
 function App() {
-  // const data = [
-  //   {
-  //     svg: (
-  //       <svg
-  //         xmlns="http://www.w3.org/2000/svg"
-  //         width="85"
-  //         height="86"
-  //         viewBox="0 0 85 86"
-  //         fill="none"
-  //       >
-  //         <path
-  //           d="M77.1875 42.8833C77.1875 62.0407 61.6574 77.5708 42.5 77.5708C23.3426 77.5708 7.8125 62.0407 7.8125 42.8833C7.8125 23.7259 23.3426 8.1958 42.5 8.1958C61.6574 8.1958 77.1875 23.7259 77.1875 42.8833Z"
-  //           stroke="#89DB7B"
-  //           stroke-width="5"
-  //         />
-  //         <path
-  //           d="M42.5 19.8623L49.4062 33.8519L64.8125 36.154L53.6562 46.9561L56.3125 62.3623L42.5 55.1019L28.6875 62.3623L31.3438 46.9561L20.1875 36.154L35.5938 33.8519L42.5 19.8623Z"
-  //           fill="#89DB7B"
-  //         />
-  //       </svg>
-  //     ),
-  //     para: 'kashif afraz',
-  //     headi: 'im leanig in rawalpindi'
-  //   },
-
-  //   {
-  //     svg: (
-  //       <svg
-  //         xmlns="http://www.w3.org/2000/svg"
-  //         width="85"
-  //         height="86"
-  //         viewBox="0 0 85 86"
-  //         fill="none"
-  //       >
-  //         <path
-  //           d="M77.1875 42.8833C77.1875 62.0407 61.6574 77.5708 42.5 77.5708C23.3426 77.5708 7.8125 62.0407 7.8125 42.8833C7.8125 23.7259 23.3426 8.1958 42.5 8.1958C61.6574 8.1958 77.1875 23.7259 77.1875 42.8833Z"
-  //           stroke="#89DB7B"
-  //           stroke-width="5"
-  //         />
-  //         <path
-  //           d="M42.5 19.8623L49.4062 33.8519L64.8125 36.154L53.6562 46.9561L56.3125 62.3623L42.5 55.1019L28.6875 62.3623L31.3438 46.9561L20.1875 36.154L35.5938 33.8519L42.5 19.8623Z"
-  //           fill="#89DB7B"
-  //         />
-  //       </svg>
-  //     ),
-  //     para: 'wasif afraz',
-  //     headi: 'im leanig in rawalpindi to'
-  //   }
-  // ];
 
   return (
-    <div className="app_div">
+    <main >
       <Header />
       <AfterHeader/>
       <Services />
@@ -69,7 +20,7 @@ function App() {
       <WhatPeopleSay />
       <Appointment />
       <Footer />
-    </div>
+    </main>
   );
 }
 
--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,13 +1,35 @@
 import React from "react";
 import "./Appointment.css";
 import AppointmentImg from "../assets/Appoinment_img.png";
+import arrowImage from "../assets/Vector.png";
 const Appointment = () => {
   return (
     <div className="appointment_container">
       <div className="appointment_card_container">
         <img src={AppointmentImg} className="appointment_card_img" />
         <div className="appointment_form">
-          <div></div>
+          <div className="appointment_heading">Free Consultation</div>
+          <div className="appointment_form_input">
+            <input className="appointment_input" placeholder="Full Name*" />
+            <input
+              className="appointment_input"
+              placeholder="I'm interested in*"
+            />
+          </div>
+
+          <div className="appointment_form_input">
+            <input className="appointment_input" placeholder="Full Name*" />
+            <input
+              className="appointment_input"
+              placeholder="I'm interested in*"
+            />
+          </div>
+          <button className="appointment_btn">
+            <div className="appointment_btn_content">
+              Get a Free Consultation
+              <img className="appointment_arrow_img" src={arrowImage} alt="" />
+            </div>
+          </button>
         </div>
       </div>
     </div>
